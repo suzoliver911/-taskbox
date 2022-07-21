45f7f9b74ed8f2222b8213b7a500d189d96d757f
Suz <oliver.suzanne2@mayo.edu>
2022-07-21 16:18:42 +0000

    Composite Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
-  declarations: [AppComponent, TaskComponent],
+  declarations: [AppComponent, TaskComponent, TaskListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
